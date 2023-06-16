5bb70e3344023171e6c79967d456cbe66ff9f63e
simoxii48 <mohahejjam@gmail.com>
2023-06-16 03:57:06 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for(a = 'a'; a <= 'z'; a++)
 		{
-			if(a != 'q' || a != 'e')
+			if(a != 'q' && a != 'e')
 				putchar(a);
 		}
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
