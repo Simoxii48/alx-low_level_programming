fabab23136b20e26ec31baf3047bd82a6dbf3788
simoxii48 <mohahejjam@gmail.com>
2023-06-16 03:59:16 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,10 @@ int main(void)
 	int a;
 
 	for(a = 'a'; a <= 'z'; a++)
-		{
-			if(a != 'q' && a != 'e')
-				putchar(a);
-		}
-		putchar('\n');
+	{
+		if(a != 'q' && a != 'e')
+			putchar(a);
+	}
+	putchar('\n');
 	return (0);
 }
