393d8f985fbd406a65c801e5e0d707a1b09ec8d3
simoxii48 <mohahejjam@gmail.com>
2023-06-20 11:57:11 -0700

    fixed betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
-#include"main.h"
+#include "main.h"
+
 /**
- *print_alphabet print all lowercase alphabet and new line 
+ * print_alphabet - print alphabets in lowercase
  */
 
 void print_alphabet(void)
 {
 	char i;
-	for (i = 'a'; i <= 'z'; i++)	
+
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-	
+
 	_putchar('\n');
 }
+
