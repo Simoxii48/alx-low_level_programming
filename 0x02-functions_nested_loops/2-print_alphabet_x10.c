33aa27592a93a916f392d69f5dfb90655ff128f7
simoxii48 <mohahejjam@gmail.com>
2023-06-20 12:24:58 -0700

    betty style done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
- *print_alphabet_10x will print from a to z in 10 lines
- *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 
 void print_alphabet_x10(void)
