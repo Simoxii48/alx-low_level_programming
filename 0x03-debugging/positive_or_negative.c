67a44ec94f333ca93b11c7945e1f59c4063a7a24
simoxii48 <mohahejjam@gmail.com>
2023-06-22 01:00:52 -0700

    folder name fixed

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+* main - tests function that prints if integer is 0
+* Return: 0
+*/
+
+int positive_or_negative(int n)
+{
+	if (n == 0)
+	{	
+		printf("%d is zero\n", n);
+	}	
+	return (0);
+}
