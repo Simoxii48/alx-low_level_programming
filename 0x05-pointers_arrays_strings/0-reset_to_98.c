ce12f40977d061e5b606bfa0430942655eab5697
simoxii48 <mohahejjam@gmail.com>
2023-06-26 02:43:54 -0700

    betty doc

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *reset_to_98 - to reset any value of n to 98
+ * reset_to_98 - to reset any value of n to 98
+ * @n: is the ineger to restore
  *
  */
 void reset_to_98(int *n)
