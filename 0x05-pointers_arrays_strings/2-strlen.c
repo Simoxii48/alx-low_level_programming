ef8dfad969ae2431fae4a3109b47e48634bb611f
simoxii48 <mohahejjam@gmail.com>
2023-06-26 02:52:51 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include"main.h"
+#include<string.h>
+/**
+ *_strlen - function to know the length of the string comins as char*
+ * @s: is the name of the string
+ *
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
